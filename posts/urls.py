c6cfaaba21b1dcf2c8ef2ac9deecc61eb34abdec
anonshubh <shubhpathak07@gmail.com>
2020-04-16 17:57:59 +0530

    Initial Commit

---
+++ posts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import PostList,PostDetail,UserDetail,UserList,UserViewSet,PostViewSet
+from rest_framework import routers
+'''
+urlpatterns=[
+    path('',PostList.as_view()),
+    path('<int:pk>',PostDetail.as_view()),
+    path('users/',UserList.as_view()),
+    path('users/<int:pk>',UserDetail.as_view()),
+]
+'''
+# Routers
+router = routers.SimpleRouter()
+router.register('users',UserViewSet,basename='users')
+router.register('',PostViewSet,basename='posts')
+
+urlpatterns = router.urls
